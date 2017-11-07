09298b08ed2d58d72f8a1dd915ef895d1398035e
alec aivazis <alec@aivazis.com>
2017-11-06 21:44:11 -0800

    added client starter

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+// external imports
+import React from 'react'
+import ReactDOM from 'react-dom'
+// local imports
+import { Root } from './views'
+
+ReactDOM.render(<Root/>, document.getElementById('root'))
---
+++ client/src/views/Root/index.js
@@ -0,0 +1,26 @@
+// external imports
+import React from "react";
+import { Card, CardGrid } from "quark-web";
+import { Text } from "react-native-web";
+
+const Root = () => (
+  <CardGrid>
+    <Card>
+      <Text>hello</Text>
+    </Card>
+    <Card>
+      <Text>hello</Text>
+    </Card>
+    <Card>
+      <Text>hello</Text>
+    </Card>
+    <Card>
+      <Text>hello</Text>
+    </Card>
+    <Card>
+      <Text>hello</Text>
+    </Card>
+  </CardGrid>
+);
+
+export default Root;
