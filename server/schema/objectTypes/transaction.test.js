f239214bfc0fc3ad4190db08b36083c93184627a
alec aivazis <alec@aivazis.com>
2017-11-12 22:22:58 -0800

    added tests for transaction associations

--- server/schema/objectTypes/transaction.test.js
+++ server/schema/objectTypes/transaction.test.js
@@ -18,10 +18,62 @@ describe('API', () => {
             const result = await graphql(
                 schema,
                 `
+                    query {
+                        node(id: "${id}") {
+                          ... on Transaction {
+                            amount
+                          }
+                        }
+                    }
+                `
+            )
+
+            // make sure there aren't any errors
+            expect(result.errors).toBeUndefined()
+            // make sure we found the right transaction
+            expect(result.data.node.amount).toEqual(1)
+        })
+
+        it('can compute the fund associated with a transaction', async () => {
+            // create a global ID from the id we are looking for
+            const id = toGlobalId('Transaction', 1)
+
+            // find the transaction via the node endpoint
+            const result = await graphql(
+                schema,
+                `
+                    query {
+                        node(id: "${id}") {
+                          ... on Transaction {
+                            fund {
+                              id
+                            }
+                          }
+                        }
+                    }
+                `
+            )
+
+            // make sure there aren't any errors
+            expect(result.errors).toBeUndefined()
+            // make sure we found the right transaction
+            expect(result.data.node.fund.id).toEqual(toGlobalId('Fund', 1))
+        })
+
+        it('can compute the project associated with a transaction', async () => {
+            // create a global ID from the id we are looking for
+            const id = toGlobalId('Transaction', 1)
+
+            // find the transaction via the node endpoint
+            const result = await graphql(
+                schema,
+                `
                   query {
                       node(id: "${id}") {
                         ... on Transaction {
-                          amount
+                          project {
+                            id
+                          }
                         }
                       }
                   }
@@ -31,7 +83,7 @@ describe('API', () => {
             // make sure there aren't any errors
             expect(result.errors).toBeUndefined()
             // make sure we found the right transaction
-            expect(result.data.node.amount).toEqual(1)
+            expect(result.data.node.project.id).toEqual(toGlobalId('Project', 1))
         })
     })
 })
