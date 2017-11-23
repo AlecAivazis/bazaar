ffec889747ecbde8993a56f03d1ed90e3d2f7414
alec aivazis <alec@aivazis.com>
2017-11-22 18:58:47 -0800

    issues are now sorted in decending order by vote

--- client/src/views/ProjectDetails/ProjectOverview/ProjectIssueTable/index.js
+++ client/src/views/ProjectDetails/ProjectOverview/ProjectIssueTable/index.js
@@ -20,6 +20,13 @@ const ProjectIssueTable = ({ repository, style }: Props) => {
         throw new Error('Cannot find issues in the repository')
     }
 
+    // sort the issues by
+    const issues = repository.issues.edges
+        .filter(edge => edge && edge.node)
+        .map(({ node }) => node)
+        .sort((a, b) => b.reactions.totalCount - a.reactions.totalCount)
+    console.log(issues)
+
     // guards
     return (
         <View style={{ ...styles.container, ...style }}>
@@ -27,9 +34,7 @@ const ProjectIssueTable = ({ repository, style }: Props) => {
                 <Title style={styles.header}>Open Issues ({repository.issues.totalCount})</Title>
             </View>
             <View style={styles.issueRowContainer}>
-                {repository.issues.edges.map(
-                    edge => edge && edge.node && <TableRow issue={edge.node} key={edge.node.id} />
-                )}
+                {issues.map(issue => <TableRow issue={issue} key={issue.id} />)}
             </View>
         </View>
     )
@@ -44,6 +49,9 @@ export default createFragmentContainer(
                 edges {
                     node {
                         id
+                        reactions(first: 0, content: THUMBS_UP) {
+                            totalCount
+                        }
                         ...ProjectIssueTableRow_issue
                     }
                 }
