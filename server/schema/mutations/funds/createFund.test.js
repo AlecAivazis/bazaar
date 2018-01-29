e66d8a9928cbb1301336287873376b730c789385
Alec Aivazis <alec@aivazis.com>
2018-01-29 02:13:07 -0800

    added failing test

--- server/schema/mutations/funds/createFund.test.js
+++ server/schema/mutations/funds/createFund.test.js
@@ -5,46 +5,96 @@ import { fromGlobalId } from 'graphql-relay'
 import database, { cleanDb, migrateDb } from '../../../database'
 import schema from '../..'
 
-describe('API', () => {
-    describe('Mutations', () => {
-        beforeEach(migrateDb)
-        afterEach(cleanDb)
+beforeEach(migrateDb)
+afterEach(cleanDb)
 
-        describe('CreateFund', () => {
-            test('can create a fund', async () => {
-                // create a fund with a known repoID
-                const result = await graphql(
-                    schema,
-                    `
-                        mutation {
-                            CreateFund(input: { name: "My Fund", address: "1234" }) {
-                                fund {
-                                    name
-                                    address
-                                }
-                            }
-                        }
-                    `
-                )
+test('can create a fund', async () => {
+    // create a fund with a known repoID
+    const result = await graphql(
+        schema,
+        `
+            mutation {
+                CreateFund(input: { name: "My Fund", address: "1234" }) {
+                    fund {
+                        name
+                        address
+                    }
+                }
+            }
+        `
+    )
 
-                // make sure there are no errors
-                expect(result.errors).toBeUndefined()
+    // make sure there are no errors
+    expect(result.errors).toBeUndefined()
 
-                // make sure we got the project back
-                expect(result.data.CreateFund.fund).toEqual({
-                    name: 'My Fund',
-                    address: '1234'
-                })
-
-                // make sure we added the project to the database
-                const funds = await database('funds')
-                    .select('address')
-                    .where({ name: 'My Fund' })
-
-                // make sure we made it
-                expect(funds).toHaveLength(1)
-                expect(funds[0].address).toEqual('1234')
-            })
-        })
+    // make sure we got the project back
+    expect(result.data.CreateFund.fund).toEqual({
+        name: 'My Fund',
+        address: '1234'
     })
+
+    // make sure we added the project to the database
+    const funds = await database('funds')
+        .select('address')
+        .where({ name: 'My Fund' })
+
+    // make sure we made it
+    expect(funds).toHaveLength(1)
+    expect(funds[0].address).toEqual('1234')
+})
+
+test('can create a fund with constraints', async () => {
+    // create a fund with a known repoID
+    const result = await graphql(
+        schema,
+        `
+            mutation {
+                CreateFund(
+                    input: {
+                        name: "My Fund123"
+                        address: "1234"
+                        constraints: { maxStars: 10, minStars: 1, language: "JavaScript" }
+                    }
+                ) {
+                    fund {
+                        name
+                        address
+                    }
+                }
+            }
+        `
+    )
+
+    // make sure there are no errors
+    expect(result.errors).toBeUndefined()
+
+    // make sure we got the project back
+    expect(result.data.CreateFund.fund).toEqual({
+        name: 'My Fund123',
+        address: '1234'
+    })
+
+    // make sure we added the project to the database
+    const [fund] = await database('funds')
+        .select('id')
+        .where({ name: 'My Fund123' })
+
+    // make sure we added a constraint for the language
+    expect(
+        (await database('constraints')
+            .where({ field: 'language', bound: 'equals', value: 'JavaScript', fundId: fund.id })
+            .count())['count(*)']
+    ).toEqual(1)
+
+    expect(
+        (await database('constraints')
+            .where({ field: 'stars', bound: 'lessThan', value: '10', fundId: fund.id })
+            .count())['count(*)']
+    ).toEqual(1)
+
+    expect(
+        (await database('constraints')
+            .where({ field: 'stars', bound: 'greaterThan', value: '1', fundId: fund.id })
+            .count())['count(*)']
+    ).toEqual(1)
 })
